8617eb6621cd1e0ef6a9b2a95d8670e416a196ab
Gabriel Ramos <gabrielramos791@gmail.com>
2024-08-24 14:45:37 -0300

    fix phpstan

--- tests/Pest.php
+++ tests/Pest.php
@@ -26,9 +26,9 @@ uses(TestCase::class)->in('Feature', 'Unit');
 |
 */
 
-expect()->extend('toBeOne', function () {
-    return $this->toBe(1);
-});
+// expect()->extend('toBeOne', function () {
+//     return $this->toBe(1);
+// });
 
 /*
 |--------------------------------------------------------------------------
@@ -41,7 +41,7 @@ expect()->extend('toBeOne', function () {
 |
 */
 
-function something()
-{
-    // ..
-}
+// function something()
+// {
+//     // ..
+// }
